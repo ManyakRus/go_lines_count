33d56ce821b373ec1f0570221c8a1a534fc0c79a
Nikitin Aleksandr <nikitinav@atomsbt.ru>
2024-03-14 17:33:57 +0300

    готово

--- internal/create_file_txt/create_file_txt.go
+++ internal/create_file_txt/create_file_txt.go
@@ -3,7 +3,6 @@ package create_file_txt
 import (
 	"github.com/ManyakRus/go_lines_count/internal/config"
 	"github.com/ManyakRus/starter/log"
-	"github.com/ManyakRus/starter/micro"
 	"os"
 )
 
@@ -11,14 +10,25 @@ import (
 func SaveToFile(Text string) error {
 	var err error
 
-	dir := micro.ProgramDir()
-	Filename1 := config.Settings.FILENAME
-	Filename := dir + micro.SeparatorFile() + Filename1
+	//dir := micro.ProgramDir()
+	Filename := config.Settings.FILENAME
+	//Filename := dir + micro.SeparatorFile() + Filename1
+
+	// сменим директорию на текущую
+	err = os.Chdir(config.CurrentDirectory())
+	if err != nil {
+		log.Error("Chdir error: ", err)
+	}
+	log.Info("Chdir: ", config.CurrentDirectory())
+
+	//
 	file, err := os.Create(Filename)
 	_, err = file.Write([]byte(Text))
 	if err != nil {
 		log.Error("Write file name: ", Filename, " error: ", err)
 		return err
+	} else {
+		log.Info("Write file name: ", Filename)
 	}
 
 	return err
